7c3e0cddd1d940fdd23e2599e84992a0fcba120d
Hoang Nam <namhoangde2002@gmail.com>
2022-02-14 20:06:00 +0700

    update

--- src/main.js
+++ src/main.js
@@ -1,4 +1,12 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import Navigo from "navigo";
 
-createApp(App).mount('#app')
+const router = new Navigo("/", { linksSelector: "a" });
+
+const print = async (content, id) => {
+    document.getElementById("app").innerHTML = await content.render(id);
+    if(content.afterRender) content.afterRender(id);
+};
+
+router.on{
+
+};
